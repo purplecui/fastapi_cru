d03807c00c8986b1debdb8f9f1e9ac87ca0c3032
purplecui <gauravkhadka4593@gmail.com>
2025-02-27 00:11:23 +0545

    updated: env file and requirements

--- config/db.py
+++ config/db.py
@@ -1,11 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from dotenv import load_dotenv
+import os
 
-URL_DATABASE = "postgresql://postgres:password@localhost:5433/blog_api"
+load_dotenv()
+# URL_DATABASE = "postgresql://postgres:password@localhost:5433/blog_api"
+
+url_database = os.getenv("URL_DATABASE")
+# print(url_database)
 
 #connection to postgres
-engine = create_engine(URL_DATABASE)
+engine = create_engine(url_database)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind= engine) #binds session to postgres db engine
